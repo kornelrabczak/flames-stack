2ae681fa0cc8d9e8b597d417ab1f4f45441832bb
kornelrabczak <1546480+kornelrabczak@users.noreply.github.com>
2020-06-16 21:22:41 +0200

    Stack frames parsing logic that calculates depth, start and end time of each function from single frame

--- core/src/main/scala/com/thecookiezen/flames/FlameGraphs.scala
+++ core/src/main/scala/com/thecookiezen/flames/FlameGraphs.scala
@@ -1,5 +1,99 @@
 package com.thecookiezen.flames
 
-object FlameGraphs {
+import com.thecookiezen.flames.FrameParser.ParsedFrame
 
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+import scala.util.Try
+
+object FlameGraphs {
+  type Stack = Seq[String]
+  case class ParsingResult(ignored: Long, totalTime: Long, nodes: Seq[TimedStackFrameElement])
+  case class StackFrameElement(function: String, depth: Long)
+  case class TimedStackFrameElement(function: StackFrameElement, startTime: Long, endTime: Long)
+
+  def parseSamples(stackFrames: Iterable[String])(frameParser: String => Option[ParsedFrame]): ParsingResult = {
+    var ignored = 0
+    var totalTime: Long = 0
+    var previousStack = Seq.empty[String]
+
+    val timers = mutable.HashMap[StackFrameElement, Long]()
+    val nodes = ArrayBuffer[TimedStackFrameElement]()
+
+    for (frame <- stackFrames) {
+      frameParser(frame) match {
+        case Some((currentStack, currentSample)) =>
+          nodes ++= flow(timers, previousStack, currentStack, totalTime)
+          previousStack = currentStack
+          totalTime += currentSample
+        case None => ignored += 1
+      }
+    }
+
+    nodes ++= flow(timers, previousStack, Seq.empty, totalTime)
+
+    ParsingResult(ignored, totalTime, nodes)
+  }
+
+  private def flow(
+            nodesStartTime: mutable.Map[StackFrameElement, Long],
+            lastStack: Stack,
+            currentStack: Stack,
+            time: Long
+          ): Seq[TimedStackFrameElement] = {
+    val prev = lastStack.toIterator.buffered
+    val current = currentStack.toIterator.buffered
+    val nodes = ArrayBuffer[TimedStackFrameElement]()
+
+    var depth = 0
+    while (prev.headOption == current.headOption) {
+      prev.next()
+      current.next()
+      depth += 1
+    }
+
+    // finishes previous nodes that already ended
+    for ((last, i) <- prev.zipWithIndex) {
+      val key = StackFrameElement(function = last, depth = depth + i)
+
+      nodesStartTime
+        .remove(key)
+        .fold(println(s"Did not have start time for $key"))(nodes += TimedStackFrameElement(key, _, time))
+    }
+
+    // starts new node
+    for ((current, i) <- current.zipWithIndex) {
+      val key = StackFrameElement(function = current, depth = depth + i)
+      nodesStartTime.put(key, time)
+    }
+
+    nodes
+  }
+}
+
+object FrameParser {
+  type ParsedFrame = (Seq[String], Long)
+
+  def parse(stackFrame: String): Option[ParsedFrame] = {
+    val trimmedFrame = stackFrame.trim
+    if (trimmedFrame.isEmpty) {
+      Option.empty
+    }
+
+    val lastIndexOfSpace = trimmedFrame.lastIndexOf(' ') match {
+      case -1 => Option.empty
+      case i  => Some(i)
+    }
+
+    lastIndexOfSpace.flatMap { index =>
+      val (stack, sample) = trimmedFrame.splitAt(index)
+      val trimmedSample = sample.trim
+      if (trimmedSample.isEmpty)
+        None
+      else {
+        val sampleWithoutFraction = trimmedSample.split('.').head
+        Try((stack.trim.split(';').toSeq, sampleWithoutFraction.toLong)).toOption
+      }
+    }
+  }
 }
---
+++ core/src/test/scala/com/thecookiezen/flames/FlameGraphsTest.scala
@@ -0,0 +1,54 @@
+package com.thecookiezen.flames
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+
+class FlameGraphsTest extends AnyFunSpec with Matchers {
+
+  describe("A stack frames parser") {
+    it("should parse a frames to get stack with sample time") {
+      val stackFrame = "function1;function2;function3 1"
+
+      FrameParser.parse(stackFrame) shouldBe Some((Seq("function1", "function2", "function3"), 1))
+    }
+
+    it("should ignore fractional part of sample in a frame") {
+      val stackFrame = "function1;function2;function3 1.34ow"
+
+      FrameParser.parse(stackFrame) shouldBe Some((Seq("function1", "function2", "function3"), 1))
+    }
+
+    it("should return None for line with only white spaces") {
+      FrameParser.parse("  ") shouldBe None
+    }
+
+    it("should return None for frame without sample data") {
+      FrameParser.parse("test1;test2;test3 ") shouldBe None
+    }
+
+    it("should return None for frame without stack") {
+      FrameParser.parse(" 1") shouldBe None
+    }
+
+    it("should return number of ignored lines because of parsing error") {
+      FlameGraphs.parseSamples(Seq("f1;f2 ", "f1,f2,f3 3", ""))(FrameParser.parse).ignored shouldBe 2
+    }
+
+    it("should build a list of all of the nodes from stack frames") {
+      val stackFrames = Seq(
+        "f1 1",
+        "f1;f2 1",
+        "f1;f2;f3 1",
+        "f1; 1",
+        "f1;f4 1",
+        "f1;f5 1",
+        "a1;b1;c1 1"
+      )
+
+      val result = FlameGraphs.parseSamples(stackFrames)(FrameParser.parse)
+      result.ignored shouldBe 0
+      result.totalTime shouldBe 7
+      result.nodes.size shouldBe 8
+    }
+  }
+}
