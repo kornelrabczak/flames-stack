590d8c230c7e33858f5c994d833c9648f61d20fa
kornelrabczak <1546480+kornelrabczak@users.noreply.github.com>
2020-06-27 16:59:16 +0200

    Adds basic test for rendering an empty SVG element

--- core/src/test/scala/com/thecookiezen/flames/svg/SvgFlameGraphTest.scala
+++ core/src/test/scala/com/thecookiezen/flames/svg/SvgFlameGraphTest.scala
@@ -1,7 +1,20 @@
 package com.thecookiezen.flames.svg
 
-import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
 
-class SvgFlameGraphTest extends AnyFlatSpec {
+class SvgFlameGraphTest extends AnyFunSpec with Matchers {
+
+  it("should render an empty SVG") {
+    val svg = SvgFlameGraph(frames = Seq.empty)
+
+    svg.render(GraphConfig.default) should contain allElementsOf Seq(
+      "<svg",
+      "<desc>",
+      "<defs>",
+      "<style",
+      "<rect"
+    )
+  }
 
 }
