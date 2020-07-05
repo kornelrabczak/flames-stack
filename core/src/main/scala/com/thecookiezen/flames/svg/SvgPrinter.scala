e1aa42f69b1f5d176507ac9ac8198d2670092ba1
kornelrabczak <1546480+kornelrabczak@users.noreply.github.com>
2020-07-05 16:38:17 +0200

    Adds xmlnsXlink attribute for svg main tag

--- core/src/main/scala/com/thecookiezen/flames/svg/SvgPrinter.scala
+++ core/src/main/scala/com/thecookiezen/flames/svg/SvgPrinter.scala
@@ -14,6 +14,7 @@ object SvgPrinter {
   val svgHeader: GraphConfig => TypedTag[String] = config =>
     svgTags.svg(
       xmlns := "http://www.w3.org/2000/svg",
+      xmlnsXlink := "http://www.w3.org/1999/xlink",
       width := config.imageWidth,
       height := config.imageHeight,
       attr("onload") := "init(evt)",
